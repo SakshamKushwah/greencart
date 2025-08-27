249822f8f9d3f1304eb08c1558b07c115194a853
Saksham kushwah <eashuop91@gmail.com>
2025-08-28 02:04:22 +0530

    projected copleted

--- server/server.js
+++ server/server.js
@@ -34,7 +34,7 @@ const port = process.env.PORT || 4000;
 
 
 //allow multiple origins
-const allowedOrigins = ['http://localhost:5173']
+const allowedOrigins = ['http://localhost:5173', 'https://greencart-sk06.vercel.app']
 
 app.post('/stripe' , express.raw({type: 'application/json'}), stripeWebhook)
 
